bba56ae13b351bb508ac2050eb4d679c83f2750c
zeraMe1ster <sauhard.eg@gmail.com>
2020-01-12 23:47:25 +0545

    staging left ones

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 // Import & Execute
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv/config");
 
-// Routes
+// Middlewares (examples can be checking authentication)
+app.use("/login", () => {
+  console.log("Middleware running");
+});
+
+// Routes (use patch:update delete:deleting get:to get info post:to give the info)
 app.get("/", (req, res) => {
   res.send("This will be index");
 });
 
 // DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("DB connecetion made")
+);
 
 // Listen
 
